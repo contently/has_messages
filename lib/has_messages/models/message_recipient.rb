c3b5aae0d442df51601cf7d151889a2c5a04b586
Trevor Ewen <trevorewen@gmail.com>
2012-11-29 16:35:51 -0500

    Updated to include the as_json method that is used primarily in mobile, but can be used other places.

--- lib/has_messages/models/message_recipient.rb
+++ lib/has_messages/models/message_recipient.rb
@@ -26,25 +26,25 @@
 # * +hide+ -Hides the message from the recipient
 # * +unhide+ - Makes the message visible again
 class MessageRecipient < ActiveRecord::Base
-  belongs_to  :message
-  belongs_to  :receiver, :polymorphic => true
-  
+  belongs_to :message
+  belongs_to :receiver, :polymorphic => true
+
   scope :with_receiver, lambda { |receiver| where(:receiver_id => receiver.id, :receiver_type => receiver.class, :hidden_at => nil).joins(:message).merge(Message.sent) }
   scope :with_topic, lambda { |topic| includes(:message).merge(Message.with_topic(topic)) }
 
   validates_presence_of :message_id, :kind, :state, :receiver_id, :receiver_type
-  
+
   #attr_protected :state, :position, :hidden_at
-  
+
   before_create :set_position
   before_destroy :reorder_positions
-  
+
   # Make this class look like the actual message
-  delegate  :sender, :subject, :body, :recipients, :to, :cc, :bcc, :created_at, :thread, :topic,
-              :to => :message
-  
+  delegate :sender, :subject, :body, :recipients, :to, :cc, :bcc, :created_at, :thread, :topic,
+           :to => :message
+
   scope :visible, :conditions => {:hidden_at => nil}
-  
+
   # Defines actions for the recipient
   state_machine :state, :initial => :unread do
     # Indicates that the message has been viewed by the receiver
@@ -52,23 +52,23 @@ class MessageRecipient < ActiveRecord::Base
       transition :unread => :read, :if => :message_sent?
     end
   end
-  
+
   # Defines actions for the visibility of the message to the recipient
   state_machine :hidden_at, :initial => :visible do
     # Hides the message from the recipient's inbox
     event :hide do
       transition all => :hidden
     end
-    
+
     # Makes the message visible in the recipient's inbox
     event :unhide do
       transition all => :visible
     end
-    
+
     state :visible, :value => nil
-    state :hidden, :value => lambda {Time.now}, :if => lambda {|value| value}
+    state :hidden, :value => lambda { Time.now }, :if => lambda { |value| value }
   end
-  
+
   # Forwards this message, including the original subject and body in the new
   # message
   def forward
@@ -76,7 +76,7 @@ class MessageRecipient < ActiveRecord::Base
     message.sender = receiver
     message
   end
-  
+
   # Replies to this message, including the original subject and body in the new
   # message.  Only the original direct receivers are added to the reply.
   def reply
@@ -86,7 +86,7 @@ class MessageRecipient < ActiveRecord::Base
     message.original_message = self.message
     message
   end
-  
+
   # Replies to all recipients on this message, including the original subject
   # and body in the new message.  All receivers (sender, direct, cc, and bcc) are
   # added to the reply.
@@ -97,28 +97,41 @@ class MessageRecipient < ActiveRecord::Base
     message.bcc(bcc - [receiver])
     message
   end
-  
+
+  #Contently specific as_json method. Used, in this case, specifically for mobile.
+  def as_json(options={})
+    {
+        :id => id,
+        :subject => ((not options[:current_user].nil?) ? self.topic.try(:message_title, options[:current_user]) : subject),
+        :body => body,
+        :created_at => created_at,
+        :readable_created_at => created_at.try(:strftime, "%A, %b %d"),
+        :receiver => User.find(receiver_id).try(:as_json),
+        :image_path => ((not options[:current_user].nil?) ? self.topic.try(:image_info_for_topic, options[:current_user]).try(:[], :thumb) : nil)
+    }
+  end
+
   private
-    # Has the message this recipient is on been sent?
-    def message_sent?
-      message.sent?
+  # Has the message this recipient is on been sent?
+  def message_sent?
+    message.sent?
+  end
+
+  # Sets the position of the current recipient based on existing recipients
+  def set_position
+    if last_recipient = MessageRecipient.find(:first, :conditions => {:kind => kind, :message_id => message_id}, :order => 'position DESC')
+      self.position = last_recipient.position + 1
+    else
+      self.position = 1
     end
-    
-    # Sets the position of the current recipient based on existing recipients
-    def set_position
-      if last_recipient = MessageRecipient.find(:first, :conditions => {:kind => kind, :message_id => message_id}, :order => 'position DESC')
-        self.position = last_recipient.position + 1
-      else
-        self.position = 1
-      end
+  end
+
+  # Reorders the positions of the message's recipients
+  def reorder_positions
+    if position
+      position = self.position
+      update_attribute(:position, nil)
+      self.class.update_all('position = (position - 1)', ['message_id = ? AND kind = ? AND position > ?', message_id, kind, position])
     end
-    
-    # Reorders the positions of the message's recipients
-    def reorder_positions
-      if position
-        position = self.position
-        update_attribute(:position, nil)
-        self.class.update_all('position = (position - 1)', ['message_id = ? AND kind = ? AND position > ?', message_id, kind, position])
-      end
-    end
+  end
 end
