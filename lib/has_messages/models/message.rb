a4f748ac9022f3d61d38e9990cb03987cd7e4d16
Brian Cantrell <bcantrell@gmail.com>
2015-11-11 11:24:59 -0500

    Convert old where scope to proc

--- lib/has_messages/models/message.rb
+++ lib/has_messages/models/message.rb
@@ -1,27 +1,27 @@
 # Represents a message sent from one user to one or more others.
-# 
+#
 # == States
-# 
+#
 # Messages can be in 1 of 3 states:
 # * +unsent+ - The message has not yet been sent.  This is the *initial* state.
 # * +queued+ - The message has been queued for future delivery.
 # * +sent+ - The message has been sent.
-# 
+#
 # == Interacting with the message
-# 
+#
 # In order to perform actions on the message, such as queueing or delivering,
 # you should always use the associated event action:
 # * +queue+ - Queues the message so that you can send it in a separate process
 # * +deliver+ - Sends the message to all of the recipients
-# 
+#
 # == Message visibility
-# 
+#
 # Although you can delete a message, it will also delete it from the inbox of all
 # the message's recipients.  Instead, you can change the *visibility* of the
 # message.  Messages have 1 of 2 states that define its visibility:
 # * +visible+ - The message is visible to the sender
 # * +hidden+ - The message is hidden from the sender
-# 
+#
 # The visibility of a message can be changed by running the associated action:
 # * +hide+ -Hides the message from the sender
 # * +unhide+ - Makes the message visible again
@@ -32,66 +32,66 @@ class Message < ActiveRecord::Base
 
   belongs_to  :original_message, :class_name => 'Message'
   has_many    :recipients, -> { order('kind DESC, position ASC') }, :class_name => 'MessageRecipient', :dependent => :destroy
-  
+
   scope :with_topic, ->(topic) { where :topic_id => topic.id, :topic_type => topic.class }
   scope :with_receiver, ->(receiver) { joins(:recipients).merge(MessageRecipient.with_receiver(receiver)) }
 
   scope :sent, -> { where(:state => :sent) }
 
   validates_presence_of :state, :sender_id, :sender_type
-  
+
   after_save :update_recipients
-  
-  scope :visible, :conditions => {:hidden_at => nil}
-  
+
+  scope :visible, -> { where(hidden_at: nil) }
+
   # Define actions for the message
   state_machine :state, :initial => :unsent do
     # Queues the message so that it's sent in a separate process
     event :queue do
       transition :unsent => :queued #, :if => :has_recipients?
     end
-    
+
     # Sends the message to all of the recipients as long as at least one
     # recipient has been added
     event :deliver do
       transition [:unsent, :queued] => :sent #, :if => :has_recipients?
     end
   end
-  
+
   # Defines actions for the visibility of the message
   state_machine :hidden_at, :initial => :visible do
     # Hides the message from the recipient's inbox
     event :hide do
       transition all => :hidden
     end
-    
+
     # Makes the message visible in the recipient's inbox
     event :unhide do
       transition all => :visible
     end
-    
+
     state :visible, :value => nil
     state :hidden, :value => lambda {Time.now}, :if => lambda {|value| value}
   end
-  
+
   # Directly adds the receivers on the message (i.e. they are visible to all recipients)
   def to(*receivers)
     receivers(receivers, 'to')
   end
   alias_method :to=, :to
-  
+
   # Carbon copies the receivers on the message
   def cc(*receivers)
     receivers(receivers, 'cc')
   end
   alias_method :cc=, :cc
-  
+
   # Blind carbon copies the receivers on the message
   def bcc(*receivers)
     receivers(receivers, 'bcc')
   end
   alias_method :bcc=, :bcc
-  
+
   # Forwards this message, including the original subject and body in the new
   # message
   def forward
@@ -99,7 +99,7 @@ class Message < ActiveRecord::Base
     message.sender = sender
     message
   end
-  
+
   # Replies to this message, including the original subject and body in the new
   # message.  Only the original direct receivers are added to the reply.
   def reply
@@ -109,7 +109,7 @@ class Message < ActiveRecord::Base
     message.original_message = self
     message
   end
-  
+
   # returns the message thread, i.e. the chain of replies
   def thread
     if original_message
@@ -118,7 +118,7 @@ class Message < ActiveRecord::Base
       []
     end
   end
-  
+
   # Replies to all recipients on this message, including the original subject
   # and body in the new message.  All receivers (direct, cc, and bcc) are added
   # to the reply.
@@ -128,7 +128,7 @@ class Message < ActiveRecord::Base
     message.bcc(bcc)
     message
   end
-  
+
   private
     # Create/destroy any receivers that were added/removed
     def update_recipients
@@ -137,22 +137,22 @@ class Message < ActiveRecord::Base
           kind_recipients = recipients.select {|recipient| recipient.kind == kind}
           new_receivers = receivers - kind_recipients.map(&:receiver)
           removed_recipients = kind_recipients.reject {|recipient| receivers.include?(recipient.receiver)}
-          
+
           recipients.delete(*removed_recipients) if removed_recipients.any?
           new_receivers.each {|receiver| self.recipients.create!(:receiver => receiver, :kind => kind)}
 
           self.recipients.create!(:receiver => sender, :kind => :sender, :state => :read)
         end
-        
+
         @receivers = nil
       end
     end
-    
+
     # Does this message have any recipients on it?
     def has_recipients?
       (to + cc + bcc).any?
     end
-    
+
     # Creates new receivers or gets the current receivers for the given kind (to, cc, or bcc)
     def receivers(receivers, kind)
       if receivers.any?
